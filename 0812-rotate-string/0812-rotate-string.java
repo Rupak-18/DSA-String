ae0c98d8afad009fbcfe5f046f95bdf59366d0cc
Rupak Das <rupak.das.3002@gmail.com>
2025-03-01 11:52:48 +0530

    Time: 0 ms (100%), Space: 42 MB (15.58%) - LeetHub

---
+++ 0812-rotate-string/0812-rotate-string.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+       
+        String str = s + s;
+        if(s.length() != goal.length())
+            return false;
+
+        if(str.contains(goal)) 
+            return true;
+        else
+            return false;
+    }
+}
